ee7c4c1cc659c6170f873cd858cb0ca142e1e9be
Jason Mayer <flavorflavor@gmail.com>
2020-06-07 22:06:06 -0400

    new imags and updates

--- reactjs-toggle-list-grid-view/src/components/ProductRow/ProductRow.js
+++ reactjs-toggle-list-grid-view/src/components/ProductRow/ProductRow.js
@@ -1,7 +1,12 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Avatar from '../../assets/img2.png'
+
+import avatar1 from '../../assets/img1.png'
+import avatar2 from '../../assets/img2.png'
+import avatar3 from '../../assets/img3.png'
+import avatar4 from '../../assets/img4.png'
+import avatar5 from '../../assets/img5.png'
 
 import "./ProductRow.css";
 
@@ -9,19 +14,21 @@ import "./ProductRow.css";
 const ProductRow = (props) => {
     let btnClass = classNames('',{
         'thumbnail': props.listView,
-        ' col-xs-12 col-sm-6 col-md-4 col-lg-3 thumbnail-grid': props.gridView,
+        'col-xs-12 col-sm-6 col-md-4 col-lg-3 thumbnail-grid': props.gridView,
     });
+    
     return (
         <div className={btnClass}>
             <div className="caption">
-               <img src={Avatar} alt={props.data.name} className="avatar-image"/>
-                <h4 className="group inner list-group-item-heading list-text-block">
-                    {props.data.name}
+               <img src={props.data.avatar} alt={props.data.name} className="avatar-image"/>
+                <h4 className="list-group-item-heading">
+                    {props.data.name} <br />
+                    {props.data.title}
                 </h4>
-                <p className="group inner list-group-item-text list-group-item-id list-text-block">
-                  {props.data.title}
+                <p className="list-text-block">
+              
                 </p>
-                <p className="list-date list-text-block">
+                <p className="member_biopic">
                     {props.data.member_biopic}
                 </p>
                 <p className="published-color" onClick={props.handleGrid}>
--- reactjs-toggle-list-grid-view/src/components/WorkOrders/WorkOrders.js
+++ reactjs-toggle-list-grid-view/src/components/WorkOrders/WorkOrders.js
@@ -1,58 +1,70 @@
 const WorkOrders = [{
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/bill-rogers.jpg/jcr:content/renditions/original',
     button_text: 'See More',
     name: 'John Gregg',
     title: 'Chairman of the Board',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/scott-case.jpg/jcr:content/renditions/original',
     button_text: 'See More',
-    name: 'Dana Dhaliwal',
-    title: 'Head of Credit and Liquidity Delivery',
+    name: 'Scott Case',
+    title: 'Head of Credit',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/ellen-koebler.jpg/jcr:content/renditions/original',
     button_text: 'See More',
-    name: 'Tom Hacket',
+    name: 'Ellen Koebler',
     title: 'Head of Investment Banking',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 },  {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/Daryl-Bible.jpg/jcr:content/renditions/original',
     button_text: 'See More',
-    name: 'Mark Huffsteeler',
+    name: 'Daryl Bible',
     title: 'VP Investment Banking',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/Mike-Maguire.jpg/jcr:content/renditions/original',
     button_text: 'See More',
     name: 'Lisa Meyer',
     title: 'Head of Capital Markets',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/bill-rogers.jpg/jcr:content/renditions/original',
     button_text: 'See More',
-    name: 'Janet Jarret',
+    name: 'Bill Rogers',
     title: 'Head of Asset Finance',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/Daryl-Bible.jpg/jcr:content/renditions/original',
     button_text: 'See More',
     name: 'Glenn Stewart',
     title: 'Head of Corporate Banking',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/Donta-Wilson.jpg/jcr:content/renditions/original',
+   
     button_text: 'Work Done',
     name: 'Erin Watson',
-    title: 'Head of Credit and Liquidity Delivery',
+    title: 'Head of Liquidity Delivery',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/Donta-Wilson.jpg/jcr:content/renditions/original',
     button_text: 'See More',
-    name: 'Michael Wheeler',
-    title: 'Head of Credit and Liquidity Delivery',
+    name: 'Donta Wilson',
+    title: 'Head of Business Investments',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/ellen-Fitzsimmons.jpg/jcr:content/renditions/original',
     button_text: 'See More',
     service_date: '6/30/2016',
     service_time: '12:47 PM',
-    name: 'Robert Popols',
+    name: 'Ellen Fitzsimmons',
     title: 'Head of Credit Liquidity',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 },
 {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/ellen-Fitzsimmons.jpg/jcr:content/renditions/original',
     button_text: 'See More',
     service_date: '6/30/2016',
     service_time: '2:47 PM',
@@ -60,27 +72,29 @@ const WorkOrders = [{
     title: 'Head of Business Investments',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/ellen-Fitzsimmons.jpg/jcr:content/renditions/original',
     button_text: 'See More',
     service_date: '6/30/2016',
     service_time: '1:47 PM',
     name: 'Pat Harriston',
-    title: 'Head of Credit and Liquidity Delivery',
+    title: 'Head of Credit',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/chris-henson.jpg/jcr:content/renditions/original',
     button_text: 'Assigned',
     service_date: '6/30/2016',
     service_time: '2:47 PM',
-    name: 'James Neely',
+    name: 'Chris Henson',
     title: 'Head of Investment Banking',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/ellen-Fitzsimmons.jpg/jcr:content/renditions/original',
     button_text: 'Assigned',
-    service_date: '6/30/2016',
-    service_time: '2:17 PM',
     name: 'Rank Delfano',
     title: 'Chairman of Investment Banking',
     member_biopic: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 }, {
+    avatar: 'https://www.truist.com/content/dam/truist/us/en/images/leadership/ellen-Fitzsimmons.jpg/jcr:content/renditions/original',
     button_text: 'See More',
     name: 'Mike Diamond',
     title: 'VP of Liquidity Delivery',
